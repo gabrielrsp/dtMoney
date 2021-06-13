166c6ca00b8df0648a93207265a443866ae22f3f
gabriel <gabrielrsp18@gmail.com>
2021-06-13 15:33:11 -0300

    create transaction context hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboar';
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { useState } from 'react';
-import { TransactionsProvider } from './TransactionsContext';
+import { TransactionsProvider } from './hooks/useTransactions';
 
 export function App() {
 
